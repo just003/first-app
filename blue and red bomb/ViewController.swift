80043b11c7b3deb75b37300a6320138fbe60c4b9
desmond <desmond@149-161-141-185.dhcp-bl.indiana.edu>
2016-03-27 22:22:36 -0400

    Initial Commit

---
+++ blue and red bomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blue and red bomb
+//
+//  Created by desmond on 27/3/16.
+//  Copyright © 2016 desmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ blue and red bombTests/blue_and_red_bombTests.swift
@@ -0,0 +1,36 @@
+//
+//  blue_and_red_bombTests.swift
+//  blue and red bombTests
+//
+//  Created by desmond on 27/3/16.
+//  Copyright © 2016 desmond. All rights reserved.
+//
+
+import XCTest
+@testable import blue_and_red_bomb
+
+class blue_and_red_bombTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
