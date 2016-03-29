cb4cb8b8072d20859b5c37008e09fabd3e156dad
desmond <desmond@149-161-143-106.dhcp-bl.indiana.edu>
2016-03-28 23:26:56 -0400

    added new files

--- blue and red bomb/ViewController.swift
+++ blue and red bomb/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bluebomb: UIImageView!
+    @IBOutlet weak var redbomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func hideblue(sender: AnyObject) {
+        bluebomb.hidden = true
+    }
+    
+    @IBAction func hidered(sender: AnyObject) {
+        redbomb.hidden = true
     }
 
 
